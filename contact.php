516e99359f9725ae43b43ccbadcf379e0e09faf1
jani <jani.kellomaki@edu.savonia.fi>
2020-04-12 17:39:56 +0300

    first commit

---
+++ contact.php
@@ -0,0 +1,34 @@
+<html class="fullpage">
+<head>
+<link rel="stylesheet" type="text/css" href="sivutyyli.css">
+</head>
+<body>
+ <header class="header">
+  <nav>
+   <ul>
+   <img src="logo2.png" class="logo" width="100" height="120">
+    <li><a href="/sivu/websivu">Home</a></li>
+    <li><a href="/sivu/contact">Contact</a></li>
+   </ul>
+  </nav> 
+ </header>
+<main>
+	<div class="main">
+		<h2>About</h2>
+		<div class="inner">
+		<a>
+			Company was built in January 2020, because world needs better and more scalable automatic planting system. Product scales from small herb planting to huge plantations in Afrika. Many places are bad for planting and that’s why the planting system is required.
+
+Our product is automated planting system for bigger and smaller plants. Product makes planting easy and growing easier for everyone, even someone with zero experience on planting can plant he’s own plants and herbs. Product works on its own and you are able to configure settings via mobile application. Planting system automatically waters the plants and regulates the amount of light you give to the plants.
+
+At first the planting system will be only for smaller plantation, later we will start working on the scalability so the watering system could be installed to bigger plantations such as greenhouse. Main goes before the summer is to make small automatic planting system for herbs.
+		
+		</a>
+		</div>
+	</div>
+</main>
+<footer>
+<a>My little farm</a>
+</footer>
+</body>
+</html>
